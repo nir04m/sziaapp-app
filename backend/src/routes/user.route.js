23dba31f964c3715dc59759794a4a7fa1d5bbb07
Miro <mairoenwa@gmail.com>
2025-07-09 16:59:51 -0600

    users routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,1 +1,23 @@
 import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getFriendRequests, getRecommendedUsers, sendFriendRequest} from '../controllers/user.controller.js'
+import { acceptFriendRequest, getOutgoingFriendReqs, getMyFriends} from '../controllers/user.controller.js'
+
+const router = express.Router();
+
+//apply auth middleware to all routes
+router.use(protectRoute)
+
+router.get('/',getRecommendedUsers)
+router.get('/friends', getMyFriends)
+
+router.post('/friend-request/:id', sendFriendRequest);
+router.put('/friend-request/:id/accept', acceptFriendRequest)
+
+router.get('/friend-requests', getFriendRequests);
+router.get('/outgoing-friend-requests', getOutgoingFriendReqs);
+
+
+
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -18,6 +18,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/chat', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
